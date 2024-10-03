2c298d2eb0497ce8df1da287944c85cb7ecfdd5f
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-10-03 21:58:19 +0700

    update

--- src/components/Sections/About/FirstSection.jsx
+++ src/components/Sections/About/FirstSection.jsx
@@ -7,7 +7,7 @@ const FirstSection = () => {
     >
       <section className="flex flex-col justify-center items-center md:items-start md:w-[50%] lg:pt-[60px] lg:pb-24 xl:pb-32 xl:pt-[100px]">
         <h1
-          className={`text-2xl px-4 md:text-4xl lg:text-6xl xl:text-[70px] text-center md:text-start  lg:leading-none font-bold font-playfair text-primary w-[175px] md:w-full xl:w-[514px] md:pr-20 lg:pr-0`}
+          className={`text-2xl px-4 md:px-0 md:text-4xl lg:text-[36px] text-center  md:md:text-start  lg:leading-none font-bold font-playfair text-primary w-[175px]  border border-black md:md:w-full xl:w-[514px] md:pr-20 lg:pr-0`}
         >
           Temukan Buket Byatiq
         </h1>
@@ -17,72 +17,67 @@ const FirstSection = () => {
           spesial Anda lebih berkesan.
         </p>
         {/* SOSMED */}
-        <div className="flex flex-col items-center justify-center gap-4 w-full mt-8">
-          <div className="flex gap-7 justify-center w-full">
-            <div className="flex items-center gap-2">
-              <a href="https://www.instagram.com/buket_byatiq" target="_blank">
-                <img
-                  src="svg/instagram.svg"
-                  alt="ig icon"
-                  className="w-[25px] h-[25px]"
-                />
-              </a>
-              <p className="text-xs">buket_byatiq</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <a href="https://www.tiktok.com/@buket_byatiq" target="_blank">
-                <img
-                  src="svg/tiktok.svg"
-                  alt="tiktok icon"
-                  className="w-[25px] h-[25px]"
-                />
-              </a>
-              <p className="text-xs">buket_byatiq</p>
-            </div>
+        <div className="grid grid-cols-2 place-items-center gap-3 px-8 w-full mt-8 hp375:px-10 
+        md:px-0 md:place-items-start md:pe-14 border border-black">
+          <div className="flex items-center gap-2 w-[110px] border border-black">
+            <a href="https://www.instagram.com/buket_byatiq" target="_blank">
+              <img
+                src="svg/instagram.svg"
+                alt="ig icon"
+                className="w-[25px] h-[25px]"
+              />
+            </a>
+            <p className="text-xs">buket_byatiq</p>
           </div>
-          <div className="flex gap-6 justify-center w-full">
-            <div className="flex items-center gap-2">
-              <a
-                href="https://www.facebook.com/titik.thututthayanxthidia?locale=id_ID"
-                target="_blank"
-              >
-                <img
-                  src="svg/facebook.svg"
-                  alt="ig icon"
-                  className="w-[25px] h-[25px]"
-                />
-              </a>
-              <p className="text-xs">Titik Purnama</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <a
-                href="https://shopee.co.id/atik1933?entryPoint=ShopBySearch&searchKeyword=buket_byatiq"
-                target="_blank"
-              >
-                <img
-                  src="svg/shoppe.svg"
-                  alt="shoppe icon"
-                  className="w-[25px] h-[25px]"
-                />
-              </a>
-              <p className="text-xs">buket_byatiq</p>
-            </div>
+          <div className="flex items-center gap-2 w-[110px] border border-black">
+            <a href="https://www.tiktok.com/@buket_byatiq" target="_blank">
+              <img
+                src="svg/tiktok.svg"
+                alt="tiktok icon"
+                className="w-[25px] h-[25px]"
+              />
+            </a>
+            <p className="text-xs">buket_byatiq</p>
           </div>
-          <div className="flex justify-start w-full ps-[42px] hp375:ps-[50px]">
-            <div className="flex items-center gap-2">
-              <a href="https://Wa.me/6282124726241" target="_blank">
-                <img
-                  src="svg/whatsapp.svg"
-                  alt="whatsapp icon"
-                  className="w-[25px] h-[25px]"
-                />
-              </a>
-              <p className="text-xs">082124726241</p>
-            </div>
+          <div className="flex items-center gap-2 w-[110px] border border-black">
+            <a
+              href="https://www.facebook.com/titik.thututthayanxthidia?locale=id_ID"
+              target="_blank"
+            >
+              <img
+                src="svg/facebook.svg"
+                alt="ig icon"
+                className="w-[25px] h-[25px]"
+              />
+            </a>
+            <p className="text-xs">Titik Purnama</p>
+          </div>
+          <div className="flex items-center gap-2 w-[110px] border border-black">
+            <a
+              href="https://shopee.co.id/atik1933?entryPoint=ShopBySearch&searchKeyword=buket_byatiq"
+              target="_blank"
+            >
+              <img
+                src="svg/shoppe.svg"
+                alt="shoppe icon"
+                className="w-[25px] h-[25px]"
+              />
+            </a>
+            <p className="text-xs">buket_byatiq</p>
+          </div>
+          <div className="flex items-center gap-2 w-[110px] border border-black">
+            <a href="https://Wa.me/6282124726241" target="_blank">
+              <img
+                src="svg/whatsapp.svg"
+                alt="whatsapp icon"
+                className="w-[25px] h-[25px]"
+              />
+            </a>
+            <p className="text-xs">082124726241</p>
           </div>
         </div>
       </section>
-      <section className="grid grid-cols-2 items-end mt-6 md:w-[50%] lg:w-[45%] lg:pb-24 xl:pb-32">
+      <section className="grid grid-cols-2 items-end mt-6 md:w-[50%] lg:w-[45%] lg:pb-24 xl:pb-32 border border-black">
         <div className="w-[150px] h-[223px] lg:w-[180px] lg:h-[286px] xl:w-[215px] xl:h-[321px] justify-self-start xl:justify-self-end content-end">
           <img src="images/home/head-1.png" alt="" className="object-fill" />
         </div>
