ee2e1803620e79f66a7afccba3fcd981548c4778
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-10-03 21:27:53 +0700

    membuat dokumentation section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,17 @@
 import Navbar from "../components/Fragments/Navbar";
 import Footer from "../components/Fragments/Footer";
+import FirstSection from "../components/Sections/About/FirstSection";
+import FixedButtonWhatsapp from "../components/Fragments/FixedButtonWhatsapp";
+import DocumentationSection from "../components/Sections/About/DocumentationSection";
 
 const About = () => {
   return (
     <>
-      <Navbar />
-      <main className="min-h-screen">
-        about
+      <Navbar classname={`bg-[#DBD1BC]/20`} />
+      <main id="main" className="relative min-h-screen">
+        <FirstSection />
+        <DocumentationSection />
+        <FixedButtonWhatsapp /> 
       </main>
       <Footer />
     </>
