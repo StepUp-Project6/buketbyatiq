272e94e9561b6a2e9778fa2a146f784ac65acc1c
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-10-05 13:52:48 +0700

    menggunakan documentation section home di about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Fragments/Navbar";
 import Footer from "../components/Fragments/Footer";
 import FirstSection from "../components/Sections/About/FirstSection";
 import FixedButtonWhatsapp from "../components/Fragments/FixedButtonWhatsapp";
-import DocumentationSection from "../components/Sections/About/DocumentationSection";
+import DocumentationSection from "../components/Sections/Home/DocumentationSection";
 
 const About = () => {
   return (
