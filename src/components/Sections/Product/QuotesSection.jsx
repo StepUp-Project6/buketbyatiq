4f55f0671a23758c68c7b8f77475c4202ae9b080
Muhammad Riyadhi <mriyadhi914@gmail.com>
2024-10-08 11:23:15 +0700

    perbaiki QuotesSection

--- src/components/Sections/Product/QuotesSection.jsx
+++ src/components/Sections/Product/QuotesSection.jsx
@@ -5,10 +5,10 @@ import Paragraph from "../../Elements/Paragraph";
 
 const QuotesSection = () => {
   return (
-    <SectionLayout classname="relative h-[246px] md:h-[196px] lg:h-[263px] xl:h-[344px] flex items-center justify-center md:block bg-cover bg-center">
+    <SectionLayout classname="relative h-[246px] md:h-[196px] lg:h-[263px] xl:h-[344px] flex items-center justify-center md:block bg-cover bg-center overflow-hidden">
       {/* Gambar background untuk mobile dan desktop */}
-      <img src="/images/bgmobile.png" alt="" className="md:hidden absolute -z-20 inset-0 object-cover" />
-      <img src="/images/quotes.png" alt="" className="hidden md:block absolute -z-20 inset-0 object-cover" />
+      <img src="/images/bgmobile.png" alt="" className="md:hidden absolute -z-20 inset-0 object-cover h-full w-full" />
+      <img src="/images/quotes.png" alt="" className="hidden md:block absolute -z-20 inset-0 object-cover h-full w-full" />
 
       {/* Konten teks dan tombol */}
       <div className="relative z-10 flex flex-col items-center md:items-start justify-center max-w-2xl text-center md:text-left px-4 md:pl-2 lg:pl-4 gap-4 md:gap-2 lg:gap-5">
